52ce7342128f9d51930f201bf1f6f44f4378858d
Johnathon Theriot <johnathontheriit@gmail.com>
2018-01-05 14:28:29 -0600

    Initial Commit

---
+++ OGL - EGE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OGL - EGE
+//
+//  Created by Johnathon Theriot on 1/5/18.
+//  Copyright © 2018 Johnathon Theriot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
