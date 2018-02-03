b59e743ed230e5a522acff23420bd21290d15563
Johnathon Theriot <johnathontheriit@gmail.com>
2018-02-03 17:40:26 -0600

    Added pixel offset for shadows

--- OGL - EGE/engine/shader/ShadowProcessor.hpp
+++ OGL - EGE/engine/shader/ShadowProcessor.hpp
@@ -20,6 +20,14 @@ public:
         this->draw();
         this->frame->unbind();
     }
+    
+    std::unordered_map<std::string, StorageBuffer*> * render(Scene * scene, Dimensions * viewPort, std::unordered_map<std::string, IGLObject*> * objects) {
+        glEnable(GL_POLYGON_OFFSET_FILL);
+        glPolygonOffset(2.0f, 4.0f);
+        std::unordered_map<std::string, StorageBuffer*> * returnObjects = PostProcessor::render(scene, viewPort, objects);
+        glDisable(GL_POLYGON_OFFSET_FILL);
+        return returnObjects;
+    }
 };
 
 #endif /* ShadowProcessor_hpp */
